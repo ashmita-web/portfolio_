344580d1861ede22df9435ba7f0f09c74cc2ca66
Ashmita Luthra <ashmitaluthra@Ashmitas-MacBook-Air.local>
2024-05-05 13:58:32 +0530

    depl

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,22 @@
+export default function AboutMe() {
+    return (
+      <section id="AboutMe" className="about--section">
+        <div className="about--section--img">
+          <img src="./img/about-me.png" alt="About Me" />
+        </div>
+        <div className="hero--section--content--box about--section--box">
+          <div className="hero--section--content">
+           
+            <h1 className="skills-section--heading">About Me</h1>
+            <p className="hero--section-description">
+              I am a Computer Science Engineering undergrad at Netaji Subhas University of Technology. As my academic achievements, I have been selected as one of the top 70 candidates out of the 8500+ applicants for the LinkedIn Coaching Program’24, facilitated by LinkedIn. I have also been awarded the GHC Scholarship'24 to attend the annual Grace Hopper Celebration.
+
+            </p>
+            <p className="hero--section-description">
+            As a Joint Secretary at IEEE, NSUT I have conducted various events like DSSYWLC’24, the annual flagship event of the IEEE Delhi Section where we, as a team, conducted various seminars , quizzes like STEM Storytelling, Think Tank and workshops based on entrepreneurship. I have also conducted a 24-hour Hackathon as an Execomm member which resulted in a footfall of 50+ teams(250+ members).
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
